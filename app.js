938174d0a8ca6b82f0c1f7417a96a3a470714d9c
Mefteg <gimenez.tom@gmail.com>
2016-10-10 18:33:34 -0400

    Try to connect MongoDB

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 "use strict";
 
+var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
-var app = express();
 
 var PORT = 3002;
 
+var API_VERSION = '0.0.1';
+
+// ### DATABASE ###
+MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+  if (err) {
+    throw err;
+  }
+  db.collection('users').find().toArray(function(err, result) {
+    if (err) {
+      throw err;
+    }
+    console.log(result);
+  });
+});
+
+
+// ### SERVER ###
+
+var app = express();
+
 app.route('/user/:user_id')
 .get(function (req, res) {
-	//console.log(req.params);
+	var data = {
+		version: API_VERSION,
+	};
+
 	// get id
+	var userId = req.params.user_id;
+	if (!userId)
+	{
+		res.send(JSON.stringify(data));
+	}
+
+	data.id = userId;
+
 	// get info from database
+	data.lists = [1, 2, 5];
+
 	// send info to client
-	res.send('Hello World!');
+	res.send(JSON.stringify(data));
 });
 
 app.listen(PORT, function () {
