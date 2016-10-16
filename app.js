3d158e3005ed3a32b2485fbe96b252b649a9c1b2
Mefteg <gimenez.tom@gmail.com>
2016-10-16 14:28:45 -0400

    Add Post and Put request for one list
    Add module “body-parser”.
    After posting a new list, update related user to keep relation to the
    new list.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var MongoObjectID = require('mongodb').ObjectID;
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var PORT = 3002;
 
@@ -18,6 +19,12 @@ function CreateServer()
 {
 	APP = express();
 
+	// for POST and PUT requests (parse request.body)
+	APP.use(bodyParser.json()); // for parsing application/json
+	APP.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+	// ## USER ##
 	APP.route('/user/:user_id')
 	.get(function (req, res) {
 		// get id
@@ -49,6 +56,7 @@ function CreateServer()
 		}
 	});
 
+	// ## LIST ##
 	APP.route('/list/:list_id')
 	.get(function (req, res) {
 		// get id
@@ -67,11 +75,121 @@ function CreateServer()
 		else
 		{
 		  	var query = {"_id": new MongoObjectID(listId)};
-		  	console.log(query);
 		  	DATABASE.collection('lists').find(query).toArray(function(err, result) {
 		    	if (err == null && result.length == 1)
 		    	{
 		    		res.send(JSON.stringify(result[0]));
+			    }
+			    else
+			    {
+			    	SendError(err, req, res);
+			    }
+	  		});
+		}
+	})
+	.put(function (req, res) {
+		// get id
+		var listId = req.params.list_id;
+		if (listId == null)
+		{
+			SendError({content: "No list id."}, req, res);
+			return;
+		}
+
+		// get name
+		var listName = req.body.name;
+
+		// get new item
+		var listItem = null;
+		if (req.body.item != null)
+		{
+			listItem = JSON.parse(req.body.item);
+		}
+
+		// we need at least one data
+		if (listName == null && listItem == null)
+		{
+			SendError({content: "No name or item."}, req, res);
+			return;
+		}
+
+		// get info from database
+		if (DATABASE == null)
+		{
+			SendError(err, req, res);
+		}
+		else
+		{
+		  	var filter = {"_id": new MongoObjectID(listId)};
+		  	var update = {};
+		  	if (listName != null)
+		  	{
+		  		update = {"$set": {"name": listName}};
+		  	}
+		  	else
+		  	{
+		  		update = {"$push": {"items": listItem}};
+		  	}
+		  	DATABASE.collection('lists').update(filter, update, function(err, data) {
+		    	if (err == null)
+		    	{
+		    		res.send(JSON.stringify({"ok": data.result.ok, "data": data}));
+			    }
+			    else
+			    {
+			    	SendError(err, req, res);
+			    }
+	  		});
+		}
+	});
+
+	APP.route('/list')
+	.post(function (req, res) {
+		// get user id
+		var userId = req.body.user_id;
+		if (userId == null)
+		{
+			SendError({content: "No user id."}, req, res);
+			return;
+		}
+
+		// get name
+		var listName = req.body.name;
+		if (listName == null)
+		{
+			SendError({content: "No list name."}, req, res);
+			return;
+		}
+
+		// get info from database
+		if (DATABASE == null)
+		{
+			SendError({content: "No databse connection."}, req, res);
+		}
+		else
+		{
+		  	var query = {"name": listName};
+		  	// insert new list
+		  	DATABASE.collection('lists').insertOne(query, function(err, data) {
+		    	if (err == null && data.result != null)
+		    	{
+		    		var listId = data.insertedId;
+		    		console.log(data);
+		    		var filter = {"id": userId};
+		    		var update = {"$push": {"lists": listId}};
+		    		console.log(filter);
+		    		console.log(update);
+		    		// update user's info
+	  				DATABASE.collection('users').updateOne(filter, update, function(err, data) {
+	  					if (err == null)
+	  					{
+		    				res.send(JSON.stringify({"ok": data.result.ok, "data": data}));
+	  					}
+	  					else
+	  					{
+			    			SendError(err, req, res);
+	  					}
+		    		});
 			    }
 			    else
 			    {
